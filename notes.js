43e7b714773a0c14bf11afb8f0e769aa1eb61143
panupong <tayalone@gmail.com>
2018-01-21 15:27:29 +0700

    edit addNotes Method in notes.js

--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
 //console.log('Start note.js')
+const fs = require('fs')
+const _ = require('lodash')
 
 const addNotes = (title, body) => {
-    console.log(title, body)
+    let notes = []
+    let note = {
+        title,
+        body
+    }
+    try {
+        let notesString = fs.readFileSync('notes-data.json')
+        let notes = JSON.parse(notesString)
+    }
+    catch (e) {
+
+    }
+
+    let duplicateNotes =
+    
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
 }
 const getAll = () => {
     console.log("Get all Notes")
