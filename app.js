786cdee154934ad637bb6ff240336e38fbfb8541
panupong <tayalone@gmail.com>
2018-01-21 14:16:15 +0700

    installl 3rd party module with npm

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ console.log(`Starting app.js`)
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
+
 const notes = require('./notes')
 
-const res = notes.add(1, 2)
-console.log(res)
+console.log(_.isString(true))
+console.log(_.isString('Panupong'))
+
+let uniqeArray = _.uniq([1,2,2,3,3])
+console.log(uniqeArray)
 
 // let user = os.userInfo()
 // fs.appendFile('greetings.txt', `Hello ${user.username}! you are ${notes.age}`)
